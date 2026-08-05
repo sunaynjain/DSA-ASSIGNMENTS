c1786eeba8481687e2a91f7dd102126ae1d4a04e
sunaynjain <sunaynjain@gmail.com>
2026-08-05 12:01:54 +0530

    Create Case Study 3_Q12.py

---
+++ Case Study 3_Q12.py
@@ -0,0 +1,10 @@
+queue = ["C101", "C102", "C103", "C104"]
+
+print("Initial Queue:", queue)
+
+queue.pop(0)
+print("After deleting C101:", queue)
+
+queue.append("C105")
+
+print("Final Queue:", queue)
