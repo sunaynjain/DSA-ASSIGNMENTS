0b054c089c97811f1376d4f1ddf36d903a8dfc57
sunaynjain <sunaynjain@gmail.com>
2026-08-05 12:03:24 +0530

    Create Case Study 3_Q16.py

---
+++ Case Study 3_Q16.py
@@ -0,0 +1,10 @@
+courses = ["Python", "Java", "C++", "AI"]
+
+print("Initial Courses:", courses)
+
+courses.remove("Java")
+print("After deleting Java:", courses)
+
+courses.append("Cloud Computing")
+
+print("Updated Course List:", courses)
